d1c582aa76c3ddb4751ccc1cdcbf973fb5719e4e
Pedro <pedrosalviano170@gmail.com>
2023-09-05 07:41:25 -0300

    Class 003 pre-update

---
+++ Lista2/001.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    float num;
+
+    if (num < 0){
+        printf("\n The number is a negative one.");
+    }
+    else if (num == 0){
+        printf("\n The number is zero.");
+    }
+    else{
+        printf("\n The number is a positive one.");
+    }
+}
---
+++ teste.py
@@ -0,0 +1,7 @@
+var = 15
+var = float(var)
+print(type(var)) # <class 'float'>
+var = int(var)
+print(float(var)) # 15.0
+print(type(var))
+print(str(var)) # 15.0
