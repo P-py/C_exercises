16b07293c67e4bb3bf73f813b9663548802bf9d6
Pedro <pedrosalviano170@gmail.com>
2024-02-23 22:08:18 -0300

    ponteiros

--- Ponteiros/Ex09.c
+++ Ponteiros/Ex09.c
@@ -2,8 +2,29 @@
 #include <stdlib.h>
 #include <conio.h>
 
+#define SIZE 3
+
+int factorial(int value){
+    if (value==0){
+        return 1;
+    }
+    else{
+        return(value*factorial(value-1));
+    }
+}
 int main(){
-    
+    int vetorA[SIZE], vetorB[SIZE], *p_vetorA, *p_vetorB;
+    p_vetorA = vetorA;
+    p_vetorB = vetorB;
+    for (int i=0; i<SIZE; i++, p_vetorA++, p_vetorB++){
+        printf("Digite o elemento #%d do vetor: ", i+1);
+        scanf("%d", p_vetorA);
+        *p_vetorB = factorial(*p_vetorA);
+    }
+    p_vetorB = vetorB;
+    for (int i=0; i<SIZE; i++, p_vetorB++){
+        printf("%d\n", *p_vetorB);
+    }
     getch();
     return 0;
 }
