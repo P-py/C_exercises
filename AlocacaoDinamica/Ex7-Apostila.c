6b7e1dfec325b9a0f353ed962df980da006e10b6
Pedro <pedrosalviano170@gmail.com>
2024-03-11 20:19:23 -0300

    Final update

--- AlocacaoDinamica/Ex7-Apostila.c
+++ AlocacaoDinamica/Ex7-Apostila.c
@@ -2,37 +2,68 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
-void aloc(char **p, int count){
-	if ((*p = (char*) realloc(*p, count*sizeof(float)))==NULL){
+#define SIZE 1000
+
+void aloc(char **p, int size){
+	if ((*p = (char*) realloc(*p, size*sizeof(float)))==NULL){
 		printf("\nAllocation error.");
 		exit(0);
 	}
 	else {
-		printf("\nSuccessfully allocated @ %u", *p);
+		printf("\nSuccessfully allocated char vector with size %d @ %u", size, *p);
 	}
 }
-void view(char *p, int count){
+void store(char *p, char string[], int size){
 	int i;
-	printf("\nNORMAL LETTER LIST");
-	for (i=0; i<count; i++){
-		printf("\n%c", *(p+i));
-	}	
+	for (i=0; i<size; i++){
+		*(p+i) = string[i];
+	}
+}
+void transform(char *p, int size){
+	int iterations = size/2;
+	int iterator;
+	for (iterator = 0; iterator<iterations; iterator++){
+		char aux[1];
+		aux[0] = *(p+iterator);
+		*(p+iterator) = *(p+size-1-iterator);
+		*(p+size-1-iterator) = aux[0];
+	}
+}
+void view(char *p, int size){
+	int i;
+	printf("\n");
+	for (i = 0; i<size; i++){
+		printf("%c", *(p+i)); 
+	}
 }
 void main(){
-	char option;
-    int count = 0;
-    char *ptr = NULL;
-    printf("\nType in a name: ");
-    
-    do{
-        aloc(&ptr, count+1);
-        storeInversedCase(ptr, ptrInverted, count);
-        count++;
-        printf("\nDo you want to continue (y/n)? ");
-        scanf("%c", &option);
-        fflush(stdin);
-    }while(toupper(option)!='N');
-    view(ptr, count);
-    viewTransformed(ptrInverted, count);
+	char *ptr = NULL;
+	char string[SIZE];
+	int i;
+	printf("\nType in the string to store: ");
+	gets(string);
+	aloc(&ptr, strlen(string));
+	store(ptr, string, strlen(string));
+	transform(ptr, strlen(string));
+	view(ptr, strlen(string));
+    //char *ptr = NULL;
+    //char string[5] = {'p', 'e', 'd', 'r', 'o'};
+    //printf("\n%d", strlen(string));
+    //int i;
+    //for (i=0; i<5; i++){
+    //	printf("%c", string[i]);
+	//}
+	//int iterations = 5/2;
+	//printf("\n%d\n", iterations);
+	//for (i=0; i<iterations; i++){
+	//	char aux[1];
+	//	aux[0] = string[i];
+	//	string[i] = string[4-i];
+	//	string[4-i] = aux[0]; 
+	//}
+	//for (i=0; i<5; i++){
+    //	printf("%c", string[i]);
+	//}
 }
